fc9be3f37338e75829f147f25402bdb78a137ff9
tachi-kawa-code <tkcode41536@gmail.com>
2020-05-23 02:17:41 +0900

    MySQL where試した

--- app/db_sample/index.ts
+++ app/db_sample/index.ts
@@ -9,12 +9,40 @@ const db = new Database("mysql", {
 });
 
 db.link([Task]);
+await db.sync({ drop: true });
 
-await Task.create([{
-  name: "Task01",
-  start: "2020/01/01",
-  end: "2020/01/31",
-  target_amount: 20,
-}]);
+await Task.create([
+  {
+    name: "Task01",
+    start: "2020/01/01",
+    end: "2020/01/31",
+    target_amount: 20,
+  },
+  {
+    name: "Task02",
+    start: "2020/02/01",
+    end: "2020/02/07",
+    target_amount: 20,
+  },
+  {
+    name: "Task03",
+    start: "2020/02/03",
+    end: "2020/02/05",
+    target_amount: 20,
+  },
+  {
+    name: "Task04",
+    start: "2020/02/04",
+    end: "2020/02/04",
+    target_amount: 20,
+  },
+]);
+
+const now = "2020/02/06";
+const result = await Task.where("start", "<=", now)
+  .where("end", ">=", now)
+  .get();
+
+console.log(result);
 
 await db.close();
